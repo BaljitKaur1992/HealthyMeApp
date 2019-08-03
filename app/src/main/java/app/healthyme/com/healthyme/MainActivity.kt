8094d8e8954e6822f07b9889f0394a33fd38d84d
BaljitKaur1992 <gaganbangar14@gmail.com>
2019-08-03 15:27:14 +0530

    added welcome screen

--- app/src/main/java/app/healthyme/com/healthyme/MainActivity.kt
+++ app/src/main/java/app/healthyme/com/healthyme/MainActivity.kt
@@ -1,12 +1,34 @@
+/*
 package app.healthyme.com.healthyme
 
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.smarteist.autoimageslider.SliderView
+import com.smarteist.autoimageslider.SliderAnimations
+import com.smarteist.autoimageslider.IndicatorAnimations
+import app.healthyme.com.healthyme.adapter.SliderAdapterExample
+
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_intro)
+        val sliderView = findViewById<SliderView>(R.id.imageSlider)
+
+        val adapter = SliderAdapterExample(this)
+
+        sliderView.setSliderAdapter(adapter)
+
+        sliderView.setIndicatorAnimation(IndicatorAnimations.WORM) //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION)
+        sliderView.autoCycleDirection = SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH
+        sliderView.indicatorSelectedColor = Color.WHITE
+        sliderView.indicatorUnselectedColor = Color.GRAY
+        sliderView.scrollTimeInSec = 4 //set scroll delay in seconds :
+        sliderView.startAutoCycle()
     }
 }
+*/
