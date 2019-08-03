1e9c51e63265eb0f0226e8127838f83c4a63c189
BaljitKaur1992 <gaganbangar14@gmail.com>
2019-08-03 15:32:26 +0530

    added welcome screen another

---
+++ app/src/main/java/app/healthyme/com/healthyme/adapter/MyPagerAdapter.java
@@ -0,0 +1,30 @@
+package app.healthyme.com.healthyme.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+    ArrayList<Fragment> fragments=new ArrayList<>();
+    public MyPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragments.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragments.size();
+    }
+
+    public void addfragments(Fragment fragment)
+    {
+        fragments.add(fragment);
+
+    }
+}
---
+++ app/src/main/java/app/healthyme/com/healthyme/fragments/IntroSecondFragment.java
@@ -0,0 +1,32 @@
+package app.healthyme.com.healthyme.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import app.healthyme.com.healthyme.R;
+
+public class IntroSecondFragment  extends Fragment {
+
+    View view;
+    TextView textView1,textView2;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view=inflater.inflate(R.layout.intro_second_fragment, container, false);
+
+        textView1=(TextView)view.findViewById(R.id.notificationAlertsTextView);
+        textView2=(TextView)view.findViewById(R.id.notificationAlertsSubTitleTextView);
+
+
+
+
+        return view;
+    }
+}
